3e656e1ff48bbb31df094be3adf5eca0a3b12507
Iyosinator <iyosiasmulugeta@gmail.com>
2023-08-17 19:04:43 +0300

    Python

---
+++ Python/100 Days Python Bootcamp/Day 1/Project: Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+x = input("What's the name of the city you grew up in?")
+y = input("What is the name of a pet?")
+print('your band name could be '+ x + " "+y)
---
+++ Python/100 Days Python Bootcamp/Day 2/Exercise 3 - Life in Weeks.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+left_year =  90 - int(age)
+left_date = left_year*365
+left_week = left_year*52
+left_month =left_year*12
+print('You have,',left_date,"days,",left_week,"weeks, and",left_month,"months left.")
